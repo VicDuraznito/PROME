c751f91e3b5155f4b6b095140097a07c9b4e2ae3
VicDuraznito <garcia.victor.isw@unipolidgo.edu.mx>
2025-01-28 15:17:17 -0600

    base 2 (db.js path #4)

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/', // Configura la base si tu proyecto está en una subcarpeta de GitHub Pages
-  assetsInclude: ['**/*.JPG', '**/*.jpg']
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg']
 });
 
 
