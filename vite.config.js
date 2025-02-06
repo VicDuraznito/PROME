28fcc767301a3c0c36c04734c5a53e7754c69a96
VicDuraznito <garcia.victor.isw@unipolidgo.edu.mx>
2025-02-06 00:50:56 -0600

    yes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PROMEDAC', 
+  base: '/', 
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg']
 });
 
