d2916897b43bbe217d00349209bc264138cc0f48
VicDuraznito <garcia.victor.isw@unipolidgo.edu.mx>
2025-02-05 22:46:33 -0600

    nixpack2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Configura la base si tu proyecto está en una subcarpeta de GitHub Pages
+  base: '/', 
+  server: {
+    port: process.env.PORT || 4173, // Usa el puerto de Railway o el predeterminado de Vite
+    host: true // Permite que Railway acceda a la app
+  },
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg']
 });
-
 
